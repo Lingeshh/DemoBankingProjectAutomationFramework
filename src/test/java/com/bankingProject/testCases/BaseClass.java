4bb2339eab57c65e4b6ef35e72678071800906c5
mdshahidulislam007 <mdshahidul.cse007@gmail.com>
2021-11-22 21:15:08 +0600

    Logger added for logging

--- src/test/java/com/bankingProject/testCases/BaseClass.java
+++ src/test/java/com/bankingProject/testCases/BaseClass.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class BaseClass {
 	
@@ -13,14 +15,19 @@ public class BaseClass {
 	public String password="YqYhEdU";
 	public static WebDriver driver;
 	
+	public static Logger logger;
 	
 	@BeforeClass
 	public void setup()
 	{			
-		
 
-			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
-			driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		logger = Logger.getLogger("BankingProjectHAF");
+		PropertyConfigurator.configure("Log4j.properties");
+				
+		
 		
 	}
 	
--- src/test/java/com/bankingProject/testCases/TC_LoginTest_001.java
+++ src/test/java/com/bankingProject/testCases/TC_LoginTest_001.java
@@ -11,26 +11,34 @@ public class TC_LoginTest_001 extends BaseClass{
 	@Test
 	public void loginTest()
 	{
-		driver.get(baseURL);	
+		driver.get(baseURL);
+		logger.info("URL is oppened");
 		
 		LoginPagePO lp = new LoginPagePO(driver);
+		
 		lp.setUserName(username);
+		logger.info("inserted username");
 		
 		
 		lp.setPassword(password);
+		logger.info("inserted password");
 		
 		
 		lp.clickSubmit();
+		logger.info("clicked login button ");
 		
 		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 			Assert.assertTrue(true);
+			//System.out.println("Passed");
+			logger.info("Logging Test Passed");
 		
 		}
 		else
 		{
 			
 			Assert.assertTrue(false);
+			logger.info("Logging Test failed");
 			
 		}
 		
